c3bbcf1ee0569ff03af64ac4ba1aefc45e7db9f4
Johannes0605 <143514407+Johannes0605@users.noreply.github.com>
2025-11-18 12:54:25 +0100

    home page react

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfd2f2a6a385d44f276d39290fb2be8ba50c4fd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83624f5aa3e484dd0ae58e125ccdce6e1af74d3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
